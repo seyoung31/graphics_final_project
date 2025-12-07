2d7774b0cfe464045d881556e1cdc386459bebc1
msmithstern <max_smith-stern@brown.edu>
2025-12-07 15:30:09 -0500

    added dir

---
+++ src/camera_path/camerapath.cpp
@@ -0,0 +1,3 @@
+#include "camerapath.h"
+
+CameraPath::CameraPath() {}
---
+++ src/camera_path/camerapath.h
@@ -0,0 +1,17 @@
+#ifndef CAMERAPATH_H
+#define CAMERAPATH_H
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include "iostream"
+#include <glm/gtx/norm.hpp> // Required for glm::length2
+
+class CameraPath
+{
+public:
+    CameraPath();
+};
+
+#endif // CAMERAPATH_H
